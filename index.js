eeb3f1930201ff05aabfa7d2dd0db7f496c34a1c
NDICHU <ndichumuriithi@gmail.com>
2022-04-05 09:48:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input=document.querySelector("input")
 function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
